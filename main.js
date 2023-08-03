55a8caeedbca358bea6a727729b12b676c2dbcd5
RishaanCoding <140859519+RishaanCoding@users.noreply.github.com>
2023-08-03 11:54:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name = "Rishaan";
+var last_name = "Shah";
+
+var full_name = first_name.concat(last_name);
+console.log("Full name: " + full_name);
+
+
+var a = 9;
+var b = 2;
+
+console.log("Add = ", a+b);
+console.log("Subtract = ", a-b);
+console.log("Multiply = ", a*b);
+console.log("Divide = ", a/b);
